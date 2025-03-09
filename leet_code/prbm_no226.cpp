7dbf101454bf88fd1a25dfe317a570cedf56dc01
uesu6149 <uesu6149@github.com>
2025-03-09 21:25:26 +0900

    leetcode no226

---
+++ leet_code/prbm_no226.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* invertTree(TreeNode* root) {
+
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        TreeNode* tmpRight = invertTree(root->right);
+        TreeNode* tmpLeft = invertTree(root->left);
+
+        root->left = tmpRight;
+        root->right = tmpLeft;
+
+        return root;
+    }
+
+};
