0d8f944380de00a53cb8d1ae7a54821208754f31
uesu6149 <uesu6149@github.com>
2025-03-16 14:14:27 +0900

    leetcode no141

---
+++ leet_code/prbm_no141.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (slow != fast) {
+            if (fast == nullptr || fast->next == nullptr) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+    }
+    
+    bool hasCycle2(ListNode* head) {
+        unordered_set<ListNode*> s;
+
+        ListNode* current = head;
+        while (current != NULL) {
+            if (s.find(current) == s.end()) {
+                s.insert(current);
+            } else {
+                // Find loop
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
+};
